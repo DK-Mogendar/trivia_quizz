df0e21d428619b09e42ea7f08a082d39cc009b15
DK-Mogendar <118730482+DK-Mogendar@users.noreply.github.com>
2023-01-26 12:07:35 +0100

    grund Strucktur

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Sprach Kurs Html</title>
+<!--Bootstrap 5-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<!--Boostrap Icons-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+<!--einbindung Javascript und Css Dokument-->
+        <link rel="stylesheet" href="/assets/css/primary_style.css">  
+        <script src="/assets/js/primary.js"></script>
+</head>
+    <body>
+
+
+    </body>
+</html>
